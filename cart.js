841058e57d8ba2016d4a668e64257d075732c99c
Alyssa Askew <alyssa.d.askew@gmail.com>
2017-08-27 18:51:34 -0700

    Items are showing up in Shopping Cart and being totaled.

--- cart.js
+++ cart.js
@@ -1,9 +1,31 @@
 const cartButtons = $('button');
 
+const shoppingcart = $('#cart')
+
 const orderItems = [
-  {
-    name: 'Some Item',
-    price: 9.99
-  },
-  // ...
+  // Empty Order Items Thing!
 ];
+
+var total = 0.00
+
+cartButtons.on('click',function(event){
+  var name1 = $(this).attr('name');
+  var price1 = $(this).attr('price');
+    orderItems.push({name: name1 , price: price1});
+    console.log(orderItems);
+  $('.cart').empty()
+  for (var i=0; i < orderItems.length; i++){
+    $('.cart').append('<p>');
+    $('.cart').append(orderItems[i].price + " --- " + orderItems[i].name);
+    $('.cart').append('</p>');
+  };
+  for (var i=0; i <orderItems.length; i++){
+    total = total + parseFloat(orderItems[i].price);
+  
+  };
+  console.log(total);    
+
+});
+
+
+
